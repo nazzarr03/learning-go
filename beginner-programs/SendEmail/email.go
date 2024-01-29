6cc45ac512420415dc4317f6b0cefa98f95a7d77
nazzarr03 <nazararik585@gmail.com>
2024-01-29 16:24:02 +0300

    Added Send Email in beginner programs.

---
+++ beginner-programs/SendEmail/email.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"net/smtp"
+)
+
+func main() {
+	// Chose auth method and set it up
+	auth := smtp.PlainAuth("", "email", "password", "smtp-client")
+
+	// Email details
+	from := "email"
+	to :="email"
+	subject := "Hello there!"
+	body := "Hello there my friend!"
+
+	msg := "From: " + from + "\n" +
+		   "To: " + to + "\n" +
+		   "Subject: " + subject + "\n" +
+		   body
+
+	err := smtp.SendMail("email-client", auth, "email", []string{to}, []byte(msg))
+	if err != nil {
+		log.Fatal(err)
+	}
+}
